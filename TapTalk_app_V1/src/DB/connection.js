1a1ea26c241d9e06a631fcdce89c12d2b5756b41
Mohammed Shamel <160044480+Mohamed-shamel1@users.noreply.github.com>
2025-11-07 04:22:44 +0200

    Add railway.json + update package.json for Railway

--- TapTalk_app_V1/src/DB/connection.js
+++ TapTalk_app_V1/src/DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/TapTalk";
+    const dbURI = process.env.MONGODB_URI || "mongodb+srv://mshamel460_db_user:rrqT10vNmVgsdqLx@cluster0.hyumno8.mongodb.net/TapTalk?retryWrites=true&w=majority&appName=TapTalk";
     const result = await mongoose.connect(dbURI, {
       serverSelectionTimeoutMS: 30000
     });
